1f8d4a5b77f260e60a29a8eb085bac425a4af163
Austin R Hudson <hudsonaustin25@gmail.com>
2024-02-04 14:32:07 -0600

    Complete personal

--- SpotifyDataLoader.py
+++ SpotifyDataLoader.py
@@ -2,20 +2,31 @@
 # json file
 
 import json
+import os
 
 # Opening JSON file
-f = open('Spotify2023-2024.json', errors="ignore")
+
+
+
+directory = os.fsencode("Data")
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    filename = "data/" + filename
+    fileData = open(filename, errors="ignore")
+    spotifyData = json.load(fileData)
+    for i in spotifyData:
+        print(i["ts"])
+        print(i['master_metadata_track_name'])
+    fileData.close()
 
 # returns JSON object as
 # a dictionary
-data = json.load(f)
+
 
 # Iterating through the json
 # list
-for i in data:
-    print(i["ts"])
-    print(i['master_metadata_track_name'])
+
 
 
 # Closing file
-f.close()
